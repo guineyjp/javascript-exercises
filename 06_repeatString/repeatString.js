4df6244c859c81ad5a486cc4a0797eeab3030ef8
John <guineyjp@gmail.com>
2026-02-11 22:45:49 +0000

    Completed repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+
+//write a function that repeats the string a given number of times
+const repeatString = function(string, num) {
+    let result = "";
+    for (let i=0; i < num; i++) {
+        result = result + string // first iteration result is "" + hey. second time result is 
+    }
+
+    return result; 
 
 };
 
