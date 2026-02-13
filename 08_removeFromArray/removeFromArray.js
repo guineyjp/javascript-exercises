efab0dbe10e2192d34ba1b1bc73c3832e26a5980
John <guineyjp@gmail.com>
2026-02-13 18:06:00 +0000

    created removeFromArray function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+
+const removeFromArray = function(array, ...toRemove) {
+    return array.filter(item => !toRemove.includes(item));
+}
+arr = [1,2,3,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
